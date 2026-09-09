f8063c72b575c6b38357f36c68a07389a3a0418c
KumarShrey <kumarshrey14477@gmail.com>
2026-09-10 04:44:11 +0530

    Add Controllers

---
+++ 06-node-express/31-notes-api/controllers/noteController.js
@@ -0,0 +1,38 @@
+import notes from "../data/notes.js";
+
+export const getNotes = (req, res) => {
+    res.json(notes);
+};
+
+export const createNote = (req,res) => {
+    const newNote = {
+        id: notes.length + 1,
+        title: req.body.title,
+        content:req.body.content
+    };
+    notes.push(newNote);
+    res.status(201).json(newNote);
+}
+
+export const updateNote = (req,res) => {
+    const id = parseInt(req.params.id);
+    const note = notes.find((note)=>note.id===id);
+    if(!note){
+        return res.status(404).json({message: "Note not found" });
+    }
+    note.title = req.body.title;
+    note.content = req.body.content;
+
+    res.status(200).json(note);
+};
+
+export const deleteNote = (req,res) => {
+    const id = parseInt(req.params.id);
+    const noteIndex = notes.findIndex((note)=> note.id === id);
+
+    if(noteIndex === -1){
+        return res.status(404).json({message: "Note not found" });
+    }
+    notes.splice(noteIndex, 1);
+    res.status(200).json({message: "Note deleted successfully" });
+};
--- 06-node-express/31-notes-api/routes/noteRoutes.js
+++ 06-node-express/31-notes-api/routes/noteRoutes.js
@@ -1,52 +1,19 @@
 import express from "express";
 import notes from "../data/notes.js";
+import { getNotes, createNote, updateNote, deleteNote } from "../controllers/noteController.js";
 
 const router = express.Router();
 
 // ------------GET REQUEST---------------
-router.get("/", (req, res) => {
-  res.json(notes);
-});
+router.get("/", getNotes);
 
-router.post("/", (req, res) => {
-    const newNote = {
-        id: notes.length + 1,
-        title: req.body.title,
-        content: req.body.content
-    };
+// -------------POST REQUEST------------
+router.post("/", createNote);
 
-    notes.push(newNote);
+// -------------PUT REQUEST------------
+router.put("/:id", updateNote);
 
-    res.status(201).json(newNote);
-});
-
-router.put("/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-
-    const note = notes.find((note) => note.id === id);
-
-    if (!note) {
-        return res.status(404).json({ message: "Note not found" });
-    }
-
-    note.title = req.body.title;
-    note.content = req.body.content;
-
-    res.status(200).json(note);
-});
-
-router.delete("/:id", (req, res) => {
-    const id = parseInt(req.params.id);
-
-    const noteIndex = notes.findIndex((note) => note.id === id);
-
-    if (noteIndex === -1) {
-        return res.status(404).json({ message: "Note not found" });
-    }
-
-    notes.splice(noteIndex, 1);
-
-    res.status(200).json({ message: "Note deleted successfully" });
-});
+// -------------DELETE REQUEST------------
+router.delete("/:id", deleteNote);
 
 export default router;
