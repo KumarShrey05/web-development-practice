81e82c3e71d4c6f35e6860f7dc015751d23867de
KumarShrey <kumarshrey14477@gmail.com>
2026-09-11 22:09:35 +0530

    Add Error Handling

--- 06-node-express/31-notes-api/server.js
+++ 06-node-express/31-notes-api/server.js
@@ -1,11 +1,16 @@
 import express from "express";
 import noteRoutes from "./routes/noteRoutes.js"
+import logger from "./middleware/logger.js";
+import errorHandler from "./middleware/errorHandler.js";
 
 const app = express();
 
 app.use(express.json());
+app.use(logger);
 
 app.use("/api/notes", noteRoutes);
+
+app.use(errorHandler);
 
 
 app.listen(5000, () => {
