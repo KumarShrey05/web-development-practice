3e0de3a63e9d1439a00b8a884439d06604fd795a
KumarShrey <kumarshrey14477@gmail.com>
2026-09-12 20:26:48 +0530

    Add SQL Database

--- 06-node-express/31-notes-api/server.js
+++ 06-node-express/31-notes-api/server.js
@@ -2,11 +2,22 @@ import express from "express";
 import noteRoutes from "./routes/noteRoutes.js"
 import logger from "./middleware/logger.js";
 import errorHandler from "./middleware/errorHandler.js";
+import pool from "./config/db.js";
 
 const app = express();
 
 app.use(express.json());
 app.use(logger);
+
+
+app.get("/test-db", async (req,res,next)=>{
+  try{
+    const [rows] = await pool.query("SELECT 1");
+    res.json({message: "Database connected", result: rows });
+  } catch (error){
+    next(error);
+  }
+});
 
 app.use("/api/notes", noteRoutes);
 
