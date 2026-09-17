b449c3b60d7e9516e99c8cfc911937450ddf0b31
KumarShrey <kumarshrey14477@gmail.com>
2026-09-17 23:39:28 +0530

    File Update

--- 06-node-express/31-notes-api/server.js
+++ 06-node-express/31-notes-api/server.js
@@ -1,29 +1,39 @@
 import express from "express";
-import noteRoutes from "./routes/noteRoutes.js"
-import logger from "./middleware/logger.js";
-import errorHandler from "./middleware/errorHandler.js";
-import pool from "./config/db.js";
 
 const app = express();
- 
-app.use(express.json());
-app.use(logger);
+
+const PORT = 5000;
+
+app.get("/", (req, res) =>{
+    res.send("Notes API is running");
+});
+app.get("/api/notes", (req, res) =>{
+    res.json(notes);
+});
+app.get("/api/notes/:id", (req, res) =>{
+    const id = Number(req.params.id);
+    const note = notes.find((note)=> note.id===id);
+    if(!note) {
+        return res.status(404).json({ message: "Note not found" });
+    }
+    res.json(note);
+});
 
 
-app.get("/test-db", async (req,res,next)=>{
-  try{
-    const [rows] = await pool.query("SELECT 1");
-    res.json({message: "Database connected", result: rows });
-  } catch (error){
-    next(error);
+
+app.listen(PORT, ()=> {
+    console.log(`Server is running on port: ${PORT}`);
+});
+
+const notes = [
+  {
+    id: 1,
+    title: "Learn Express",
+    content: "Understand routes and requests"
+  },
+  {
+    id: 2,
+    title: "Practice APIs",
+    content: "Test APIs using Postman"
   }
-});
-
-app.use("/api/notes", noteRoutes);
-
-app.use(errorHandler);
-
-
-app.listen(5000, () => {
-  console.log("Server is running on port 5000");
-});
+];
